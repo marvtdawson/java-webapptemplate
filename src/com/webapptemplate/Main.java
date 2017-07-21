49d72c599101cd00d2bce802b42e83c8050cb999
Marvin Dawson <marvintdawson@gmail.com>
2017-07-21 13:27:18 -0500

    init master branch commit

---
+++ src/com/webapptemplate/Main.java
@@ -0,0 +1,20 @@
+package com.webapptemplate;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String myname;
+        myname = "Marvin";
+
+        System.out.println(myname + "\r");
+
+        int currentIncome = 10;
+        int desireIncome = 135000;
+        int totalIncome = currentIncome + desireIncome;
+        System.out.println("$" + totalIncome);
+
+        System.out.println("My current income is $" + currentIncome + ", My desire income is $" + desireIncome + "!");
+    }
+
+}
